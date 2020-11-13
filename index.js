233643a0f76cba9b1a50a42687a6b292b75138a7
Kyle Trahan <ktrahan2@gmail.com>
2020-11-13 11:11:46 -0500

    finished, could use better refactor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //tech interview practice 11/13/2020
 
 //add two arrays together. 
-//[1, 2, 3] + [1, 2, 3]
+//[1, 2, 3] + [1, 2, 3] == [2, 4 , 6]
 //if index doesn't exist then its value is 0
+//[1, 2, 3] + [1, 2, 3, 4] == [2, 4, 6, 4]
 
 const array1 = [1, 2, 3]
 const array2 = [1, 2, 3, 4]
@@ -10,17 +11,17 @@ const array2 = [1, 2, 3, 4]
 const sumArrays = (array1, array2) => {
 
     let summedArray = []
-    let biggestArray 
+    let longestArray 
     //don't set variable to string unless its a string
     let sum = 0
-
+    //make separate function to find longest array
     if (array1.length > array2.length) {
-        biggestArray = array1
+        longestArray = array1
     } else {
-        biggestArray = array2
+        longestArray = array2
     }   
    
-    for (let i = 0; i < biggestArray.length; i++) {
+    for (let i = 0; i < longestArray.length; i++) {
         if (array1[i] && array2[i]) {
 
             sum = array1[i] + array2[i]
